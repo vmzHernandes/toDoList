1e046b066c4e33fee457a7835e3df1348fda2ffa
Vitor <v.marrahernandes@gmail.com>
2023-12-14 14:20:06 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,38 @@
+//Funções 
+
+function adicionaItem() {
+    let novoItem = document.getElementById('novo-item').value;
+    var li = document.createElement(li);
+    li.className = 'list-group-item';
+    li.appendChild(document.createTextNode(novoItem));
+
+    let removerBtn = document.createElement('button');
+    removerBtn.appendChild(document.createTextNode('Remover'));
+    li.appendChild(removerBtn);
+
+    listaItems.appendChild(li);
+}
+
+function removeItem(e) {
+    if(e.target.classList.contains('btn-remove')) {
+        let li = e.target.parentElement;
+        listaItems.removeChild(li);
+    }
+}
+// Código
+
+let adicionaBtn = document.getElementById('adiciona-btn');
+let listaItems = document.getElementById('itens')
+
+adicionaBtn.addEventListener('click', adicionaItem);
+listaItems.addEventListener('click', removeItem)
+
+
+
+
+
+
+
+
+
+
